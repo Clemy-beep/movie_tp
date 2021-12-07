42842479c0e4c677a1fd93711cf08ce3cfb2e414
Clemy-beep <clementine.digny@gmail.com>
2021-12-07 16:55:14 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee+Shade&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/styles/header.css">
+    <link rel="stylesheet" href="./assets/styles/footer.css">
+    <link rel="stylesheet" href="./assets/styles/anonym.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bungee+Inline&display=swap" rel="stylesheet">
+    <title>CMDB</title>
+</head>
+
+<body>
+    <header>
+        <div id="logo">
+            <span class="material-icons-outlined">
+                movie
+            </span>
+            <span id="textlogo">CMBD</span>
+        </div>
+        <div id="sign-in">
+            <a href="#">
+                <span class="material-icons-outlined">
+                    login
+                </span>
+                <span>Sign In</span>
+            </a>
+        </div>
+        <div id="sign-up">
+            <a href="#">
+                <span class="material-icons-outlined">
+                    person_add_alt_1
+                </span>
+                <span>Sign Up</span>
+            </a>
+        </div>
+    </header>
+    <div id="welcome">
+        <h1>Welcome !</h1>
+        <p>Welcome to ClemyMovieDataBase or <span id="bold">CMBD</span> for short !</p>
+        <p>Here you can browse films, watch their trailers, browse films by gender and much more ! Have a good time exploring all our resources. </p>
+        <div id="buttons">
+            <a href="#" id="sign-in_button">
+                <span class="material-icons-outlined">
+                    login
+                </span>
+                Sign in
+            </a>
+            <a href="#" id="sign-up_button">
+                <span class="material-icons-outlined">
+                    person_add_alt_1
+                </span>
+                Sign up
+            </a>
+        </div>
+    </div>
+    <footer>
+        <a href="#">About us</a>
+        <a href="#">Contact us</a>
+        <a href="#">Terms of Use</a>
+    </footer>
+
+</body>
+
+</html>
