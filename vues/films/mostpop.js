5d1197ae9440c7b16065122b2c8f4512bbf62ee7
Clemy-beep <clementine.digny@gmail.com>
2021-12-10 15:51:45 +0100

    poster path updated if null

--- vues/films/mostpop.js
+++ vues/films/mostpop.js
@@ -38,18 +38,21 @@ $.ajax({
 
 function createCardDay(films) {
     var title = "";
+    var poster_path = "";
     var major = $("#major").html();
     if (!major) {
-        films.filter(film => film.adult === true);
+        films.filter(film => film.adult === false);
     }
     if (films.length > 0) {
         films.forEach(film => {
             if (film.title === undefined) {
                 title = film.original_name;
             } else title = film.title;
-
+            if (film.poster_path !== null) {
+                poster_path = "http://image.tmdb.org/t/p/w300/" + film.poster_path;
+            } else poster_path = "https://via.placeholder.com/300x450?text=Poster+not+found";
             console.log(film.id);
-            $("#carousel1").append(`<div class="card" id="cardDay"  onclick="window.location.replace('/vues/films/film.php?id=${film.id}')"><div class="title"><div class="rating"><span class="material-icons-outlined">star_half</span>${film.vote_average}</div><h2>${title}</h2></div><img src="http://image.tmdb.org/t/p/w300/${film.poster_path}" alt="poster"></div>`);
+            $("#carousel1").append(`<div class="card" id="cardDay"  onclick="window.location.replace('/vues/films/film.php?id=${film.id}')"><div class="title"><div class="rating"><span class="material-icons-outlined">star_half</span>${film.vote_average}</div><h2>${title}</h2></div><img src="${poster_path}" alt="poster"></div>`);
         });
 
     } else $("#carousel1").append("<h2>We have no film to show you at the moment</h2>");
